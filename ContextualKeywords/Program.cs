24e48fd3c51564d901f09ac1c8e97594b90489ee
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-12 23:00:29 +0100

    Create projects and configuration files
    - ContextualKeywords
    - ReservedIdentifiers
    - build tasks
    - run configuration (VSCode)

---
+++ ContextualKeywords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpKeywords.ContextualKeywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
